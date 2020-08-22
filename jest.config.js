b8811ead4675d786f73b687e1b9eae18eafe6ea3
Dylan Elznic <dylan.elznic@gmail.com>
2020-08-21 23:55:02 -0500

    Update Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  moduleNameMapper: {
-    '^config(.*)$': '<rootDir>/dist/config$1',
-    '^db(.*)$': '<rootDir>/dist/db$1',
-    '^routes(.*)$': '<rootDir>/dist/routes$1',
-    '^services(.*)$': '<rootDir>/dist/services$1',
-  },
+  moduleDirectories: ['node_modules', 'src'],
   preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
   testEnvironment: 'node',
 };
